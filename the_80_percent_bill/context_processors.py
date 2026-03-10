026c11abf77902f0e8645b961523545bb2c0890b
antimatterz <9204050+unstableantimatter@users.noreply.github.com>
2026-03-10 16:29:47 -0400

    Remove the donate buttons on sidebar and pledge workflow

--- the_80_percent_bill/context_processors.py
+++ the_80_percent_bill/context_processors.py
@@ -1,10 +1,6 @@
 """Context processors to inject common template variables."""
 
-from django.conf import settings
-
 
 def site_context(request):
     """Add site-wide variables to all templates."""
-    return {
-        "donation_link": getattr(settings, "DONATION_LINK", ""),
-    }
+    return {}
